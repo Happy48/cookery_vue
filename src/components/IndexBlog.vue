3460d1b360f60936adc38b14dd4ef45bbae4e2f1
LittleLittleLittleBoy <151250082@smail.nju.edu.cn>
2018-06-14 10:34:00 +0800

    首页接口调用

--- src/components/IndexBlog.vue
+++ src/components/IndexBlog.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="col-md-4 food-grid animated wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="500ms">
     <div class=" hover-fold">
-      <h4>FOOD（带博客链接）</h4>
+      <h4>{{title}}</h4>
       <div class="top">
         <div class="front face" v-bind:style="{background:urlTopStyle}"></div>
         <div class="back face">
-          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+          <p>{{description}}</p>
         </div>
       </div>
       <div class="bottom" v-bind:style="{background:urlBottomStyle}"></div>
--- src/page/Index.vue
+++ src/page/Index.vue
@@ -4,9 +4,9 @@
       <Title></Title>
       <!-- start search-->
       <div class="banner">
-        <h2 class="animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms" style="font: bold;color: white">雨后 清新 空气</h2>
+        <h2 class="animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms" style="color: white">雨后 清新 空气</h2>
         <label></label>
-        <h4 class="animated wow fadeInTop" data-wow-duration="1000ms" data-wow-delay="500ms" style="color: white;">与美食相遇在雨后炎热的六月</h4>
+        <h4 class="animated wow fadeInTop" data-wow-duration="1000ms" data-wow-delay="500ms" style="color: white;">与美食相遇在雨后炎热的{{month}}</h4>
         <div id="down">
           <a class="scroll down" href="#content-down">
             <canvas id="downCanvas" width="50" height="50">你的浏览器不支持canvas，请下载更新版本的浏览器
@@ -22,22 +22,22 @@
         <div class="container">
           <div class="content-top">
             <div class="col-md-4 content-left animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
-              <h3>六月推荐</h3>
+              <h3>{{month}}推荐</h3>
               <label><i class="glyphicon glyphicon-menu-up"></i></label>
               <span>很多选择很多选择</span>
             </div>
             <div class="col-md-8 content-right animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
-              <p>六月有点六月有点热我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃</p>
+              <p>{{month}}有点{{month}}有点热我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃我们适合做什么菜吃</p>
             </div>
             <div class="clearfix"> </div>
           </div>
           <div class="content-mid">
 
-            <IndexBlog :title="indexblogData[0].title" :url="indexblogData[0].url" :description="indexblogData[0].description">
+            <IndexBlog :title="indexblogData[0].foodTitle" :url="indexblogData[0].foodPic" :description="indexblogData[0].foodDesc">
             </IndexBlog>
-            <IndexBlog :title="indexblogData[0].title" :url="indexblogData[1].url" :description="indexblogData[0].description">
+            <IndexBlog :title="indexblogData[0].foodTitle" :url="indexblogData[1].foodPic" :description="indexblogData[0].foodDesc">
             </IndexBlog>
-            <IndexBlog :title="indexblogData[0].title" :url="indexblogData[2].url" :description="indexblogData[0].description">
+            <IndexBlog :title="indexblogData[0].foodTitle" :url="indexblogData[2].foodPic" :description="indexblogData[0].foodDesc">
             </IndexBlog>
             <div class="clearfix"></div>
           </div>
@@ -49,24 +49,24 @@
       <div class="container">
         <div class="content-top">
           <div class="col-md-4 content-left animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
-            <h3>六月热门</h3>
+            <h3>{{month}}热门</h3>
             <label><i class="glyphicon glyphicon-menu-up"></i></label>
             <span>很多热门很多热门</span>
           </div>
           <div class="col-md-8 content-right animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
-            <p>这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系</p>
+            <p>这是{{month}}的热门菜系这是{{month}}的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系这是六月的热门菜系</p>
           </div>
           <div class="clearfix"> </div>
         </div>
         <div class="news-bottom">
           <div class="news-bot">
-            <IndexHot :date="indexHotData[0].date" :comments="indexHotData[0].comments" :description="indexHotData[0].description" :url="indexHotData[0].url"></IndexHot>
-            <IndexHot :date="indexHotData[1].date" :comments="indexHotData[1].comments" :description="indexHotData[1].description" :url="indexHotData[1].url"></IndexHot>
+            <IndexHot :date="indexHotData[0].foodCreateTime" :comments="indexHotData[0].foodCollect" :description="indexHotData[0].foodTitle" :url="indexHotData[0].foodPic"></IndexHot>
+            <IndexHot :date="indexHotData[1].foodCreateTime" :comments="indexHotData[1].foodCollect" :description="indexHotData[1].foodTitle" :url="indexHotData[1].foodPic"></IndexHot>
             <div class="clearfix"> </div>
           </div>
           <div class="news-bot">
-            <IndexHot :date="indexHotData[2].date" :comments="indexHotData[2].comments" :description="indexHotData[2].description" :url="indexHotData[2].url"></IndexHot>
-            <IndexHot :date="indexHotData[3].date" :comments="indexHotData[3].comments" :description="indexHotData[3].description" :url="indexHotData[3].url"></IndexHot>
+            <IndexHot :date="indexHotData[2].foodCreateTime" :comments="indexHotData[2].foodCollect" :description="indexHotData[2].foodTitle" :url="indexHotData[2].foodPic"></IndexHot>
+            <IndexHot :date="indexHotData[3].foodCreateTime" :comments="indexHotData[3].foodCollect" :description="indexHotData[3].foodTitle" :url="indexHotData[3].foodPic"></IndexHot>
             <div class="clearfix"> </div>
           </div>
 
@@ -83,6 +83,7 @@ import Title from '@/components/Title'
 import Footer from '@/components/Footer'
 import IndexBlog from '@/components/IndexBlog'
 import IndexHot from '@/components/IndexHot'
+import api from '@/api/getData'
 
 export default {
   stores: {
@@ -90,47 +91,48 @@ export default {
   },
   data () {
     return {
+      month: '六月',
       indexblogData: [
         {
-          title: 'FOOD(带博客链接)',
-          url: '/static/images/aa.jpg',
-          description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
+          foodTitle: 'FOOD(带博客链接)',
+          foodPic: '/static/images/aa.jpg',
+          foodDesc: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
         },
         {
-          title: 'FOOD(带博客链接)',
-          url: '/static/images/aa1.jpg',
-          description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
+          foodTitle: 'FOOD(带博客链接)',
+          foodPic: '/static/images/aa1.jpg',
+          foodDesc: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
         },
         {
-          title: 'FOOD(带博客链接)',
-          url: '/static/images/aa2.jpg',
-          description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
+          foodTitle: 'FOOD(带博客链接)',
+          foodPic: '/static/images/aa2.jpg',
+          foodDesc: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
         }
       ],
       indexHotData: [
         {
-          date: '08.09.2018',
-          comments: 5,
-          description: 'here are many variations of passages of Lorem Ipsum available',
-          url: '/static/images/ne.jpg'
+          foodCreateTime: '08.09.2018',
+          foodCollect: 5,
+          foodTitle: 'here are many variations of passages of Lorem Ipsum available',
+          foodPic: '/static/images/ne.jpg'
         },
         {
-          date: '08.09.2018',
-          comments: 5,
-          description: 'here are many variations of passages of Lorem Ipsum available',
-          url: '/static/images/ne.jpg'
+          foodCreateTime: '08.09.2018',
+          foodCollect: 5,
+          foodTitle: 'here are many variations of passages of Lorem Ipsum available',
+          foodPic: '/static/images/ne.jpg'
         },
         {
-          date: '08.09.2018',
-          comments: 5,
-          description: 'here are many variations of passages of Lorem Ipsum available',
-          url: '/static/images/ne.jpg'
+          foodCreateTime: '08.09.2018',
+          foodCollect: 5,
+          foodTitle: 'here are many variations of passages of Lorem Ipsum available',
+          foodPic: '/static/images/ne.jpg'
         },
         {
-          date: '08.09.2018',
-          comments: 5,
-          description: 'here are many variations of passages of Lorem Ipsum available',
-          url: '/static/images/ne.jpg'
+          foodCreateTime: '08.09.2018',
+          foodCollect: 5,
+          foodTitle: 'here are many variations of passages of Lorem Ipsum available',
+          foodPic: '/static/images/ne.jpg'
         }
       ]
     }
@@ -141,7 +143,24 @@ export default {
     IndexBlog,
     IndexHot
   },
+  created () {
+    this.getIndexRecommend()
+  },
   methods: {
+    getIndexRecommend () {
+      api.getIndexRecommend().then(res => {
+        let data = res.data
+        this.month = data.month
+        this.indexblogData = data.recommend
+        this.indexHotData = data.hot
+      }).catch(res => {
+        console.log('错误')
+        let data = res.data
+        this.month = data.month
+        this.indexblogData = data.recommend
+        this.indexHotData = data.hot
+      })
+    }
   }
 }
 </script>
