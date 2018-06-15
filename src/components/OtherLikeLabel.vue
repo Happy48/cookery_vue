2ab99887b99292884b23c5b5a6360234ba95b44e
Green-Cherry <2423964104@qq.com>
2018-06-15 14:26:06 +0800

    你可能喜欢css修改

--- src/components/OtherLikeLabel.vue
+++ src/components/OtherLikeLabel.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="blog-bottom animated wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="500ms">
-    <h4>{{title}}</h4>
+    <h4 >{{title}}</h4>
     <div :key="item.foodPic" v-for="item in list" class="product-go">
-      <a href="single.html" class="fashion"><img class="img-responsive " :src="item.foodPic" alt=""></a>
+      <a href="single.html" class="fashion"><img class="like_img" :src="item.foodPic" alt=""></a>
       <div class="grid-product">
-        <a href="single.html" class="elit">{{item.foodTitle}}</a>
-        <p>{{item.foodDesc}}</p>
+        <div class="line-limit-length"><a href="single.html" class="elit ">{{item.foodTitle}}</a></div>
+        <p class="likelist-limit-length">{{item.foodDesc}}</p>
       </div>
       <div class="clearfix"> </div>
     </div>
